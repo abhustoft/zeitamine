4194c30a469a9aab681f0350438603aff7536044
Alf Bjørn Hustoft <abhustoft@gmail.com>
2018-10-15 21:01:55 +0200

    Timelining

--- src/House.js
+++ src/House.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import TweenMax, {Power0, Bounce, Elastic} from "gsap/TweenMax";
+import TimelineMax from 'gsap/TimelineMax';
 
 import './App.css';
 import HouseSVG from './svg/house_and_clouds.svg';
 
 class House extends Component {
     componentDidMount() {
+
         TweenMax.fromTo('.cloud1', 38, {x: -100}, {
             x: 350,
             delay: 1,
@@ -13,6 +15,7 @@ class House extends Component {
             repeatDelay: 2,
             ease: Power0.easeNone
         });
+
         TweenMax.fromTo('.cloud2', 55, {x: -50}, {
             x: 350,
             delay: 20,
@@ -20,6 +23,17 @@ class House extends Component {
             repeatDelay: 4,
             ease: Power0.easeNone
         });
+
+        const buildings = new TimelineMax({delay: 10});
+        buildings
+            .fromTo('.building5', 1.5, {y: 80}, {ease: Elastic.easeOut, y: 0}, '+=1')
+            .fromTo('.building6', 1.5, {y: 80}, {ease: Elastic.easeOut, y: 0}, '-=1')
+            .fromTo('.building1', 1.5, {y: 80}, {ease: Elastic.easeOut, y: 0}, '+=1')
+            .fromTo('.building2', 1.5, {y: 80}, {ease: Elastic.easeOut, y: 0}, '-=1')
+            .fromTo('.building4', 1.5, {y: 80}, {ease: Elastic.easeOut, y: 0}, '-=2')
+            .fromTo('.building3', 1.5, {y: 80}, {ease: Elastic.easeOut, y: 0}, '-=2')
+            .fromTo('.building5window1', 1.5, {y: 80}, {ease: Elastic.easeOut, y: 0}, '-=1')
+            .fromTo('.building5window2', 1.5, {y: 80}, {ease: Elastic.easeOut, y: 0}, '-=1');
     }
 
     render() {

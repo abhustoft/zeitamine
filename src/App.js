0a45270ce4e2314608c8c0362fd7d2c0b1c80455
Alf Bjørn Hustoft <abhustoft@gmail.com>
2018-11-11 13:12:10 +0100

    Start of puppeteer hell

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <h1 className="App-title">Welcome to React</h1>
                 <House/>
             </div>)
     }

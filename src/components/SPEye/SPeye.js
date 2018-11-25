3d1c0f73af9b2b2054bd79f5f2911e44a490baa2
Alf Bjørn Hustoft <abhustoft@gmail.com>
2018-11-25 15:23:33 +0100

    Renames

--- src/components/SPEye/SPeye.js
+++ src/components/SPEye/SPeye.js
@@ -1,29 +1,37 @@
 import React, {Component} from 'react';
 import TweenMax, {Power0} from "gsap/TweenMax";
 
-import './SPeye.css';
-import SPeyeSVG from './speye.svg';
+import './SPEye.css';
+import SPEyeSVG from './speye.svg';
 import SPEyeBottom from "./speyeBottom.svg";
 
+
 class SPEye extends Component {
+    constructor(props) {
+        super(props);
+        this.blink = () => {
+            TweenMax.to(['.eyeMask'], 0.2, {
+                // scale: 0,
+                delay: .2,
+                x: -40,
+                y: -40,
+                repeat: 1,
+                repeatDelay: 0.15,
+                ease: Power0.easeInOut,
+                yoyo:true,
+            });
+        };
+    }
+
     componentDidMount() {
-        TweenMax.to(['.eyeMask'], 0.2, {
-            // scale: 0,
-            delay: 3,
-            x: -40,
-            y: -40,
-            repeat: 1,
-            repeatDelay: 0.15,
-            ease: Power0.easeInOut,
-            yoyo:true,
-        });
+        this.blink();
     }
 
     render() {
         return (
-            <div className='SPeye'>
-                <SPEyeBottom className={'speyeBottom'}/>
-                <SPeyeSVG className={'speye'}/>
+            <div className='SPEye' onClick={this.blink}>
+                <SPEyeBottom className={'eyeBall'}/>
+                <SPEyeSVG className={'pupil'}/>
             </div>
         )
     }

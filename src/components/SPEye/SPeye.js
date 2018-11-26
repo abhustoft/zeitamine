3ade7721d2dd15424202fa16b42cae95c6e1edd2
Alf Bjørn Hustoft <abhustoft@gmail.com>
2018-11-26 19:20:27 +0100

    Snake eye blink

--- src/components/SPEye/SPeye.js
+++ src/components/SPEye/SPeye.js
@@ -1,25 +1,31 @@
 import React, {Component} from 'react';
-import TweenMax, {Power0} from "gsap/TweenMax";
+import {Power0} from "gsap/TweenMax";
 
 import './SPEye.css';
 import SPEyeSVG from './speye.svg';
 import SPEyeBottom from "./speyeBottom.svg";
+import TimelineMax from "gsap/TimelineMax";
 
 
 class SPEye extends Component {
     constructor(props) {
         super(props);
         this.blink = () => {
-            TweenMax.to(['.eyeMask'], 0.2, {
-                // scale: 0,
-                delay: .2,
-                x: -40,
-                y: -40,
-                repeat: 1,
-                repeatDelay: 0.15,
-                ease: Power0.easeInOut,
-                yoyo:true,
-            });
+            const bat = new TimelineMax({delay: 0.1});
+            bat
+                .to('.eyeMask', 0.01, {x: 1.5, rotation: -60, transformOrigin: '50% 50%'})
+                .to('.eyeMask', 0.12, {
+                    skewY: "-80deg", transformOrigin: '50% 50%',
+                    skewX: "-10deg",
+                    rotation: -10,
+                    ease: Power0.easeInOut,
+                }, '+=0.1')
+                .to('.eyeMask', 0.2, {
+                    skewY: "0deg", transformOrigin: '50% 50%',
+                    skewX: "0deg",
+                    rotation: -50,
+                    ease: Power0.easeInOut,
+                }, '+=0.1');
         };
     }
 

0f127940743d2626fe410b783a10d0d03d818b08
Alf Bjørn Hustoft <alf.hustoft@sparebank1.no>
2018-12-27 14:41:58 +0100

    React route animation, half done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { TransitionGroup, Transition } from "react-transition-group";
+import { play, exit } from './timelines'
 import './App.css';
-import House from './components/House';
-import SPEye from './components/SPEye/SPeye';
+// import House from './components/House';
+// import SPEye from './components/SPEye/SPeye';
+
+const dull = () => (<div>dull</div>);
+const full = () => (<div>full</div>);
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <House/>
-                <div className='blueBox'>
-                    <SPEye/>
+            <BrowserRouter>
+                <div className="App">
+                    <Route render={(location) => {
+                        const {key} = location;
+                        const pathname = location.location;
+                        return (
+                            <TransitionGroup component={null}>
+                                <Transition
+                                    key={key}
+                                    appear={true}
+                                    onEnter={(node, appears) => play(pathname, node, appears)}
+                                    onExit={(node, appears) => exit(node, appears)}
+                                    timeout={{enter: 750, exit: 150}}
+                                >
+                                    <Switch location={pathname}>
+                                        <Route exact path="/" component={dull}/>
+                                        <Route path="/eye" component={full}/>
+                                    </Switch>
+                                </Transition>
+                            </TransitionGroup>
+                        );
+                    }}
+                    />
                 </div>
-            </div>)
+            </BrowserRouter>
+        )
     }
 }
 

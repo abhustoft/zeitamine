002c8b7f826855a1b770594b711668e985999a71
Alf Bjørn Hustoft <alf.hustoft@sparebank1.no>
2018-12-28 11:55:36 +0100

    Simple react route animation

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import { TransitionGroup, Transition } from "react-transition-group";
-import { play, exit } from './timelines'
+import {TransitionGroup, Transition} from "react-transition-group";
+import {play, exit} from './timelines'
 import './App.css';
+import Nav from './Nav'
 // import House from './components/House';
 // import SPEye from './components/SPEye/SPeye';
 
-const dull = () => (<div>dull</div>);
-const full = () => (<div>full</div>);
+const dull = () => (<h1>
+    <div>dull</div>
+</h1>);
+const full = () => (<h1>
+    <div>full</div>
+</h1>);
+const hull = () => (<h1>
+    <div>hull</div>
+</h1>);
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
                 <div className="App">
+                    <Nav/>
                     <Route render={(location) => {
-                        const {key} = location;
+                        const key = location.location.key;
                         const pathname = location.location;
+                        console.log('Route render pathname:', pathname)
+                        console.log('Route render key:', key)
                         return (
                             <TransitionGroup component={null}>
                                 <Transition
                                     key={key}
                                     appear={true}
-                                    onEnter={(node, appears) => play(pathname, node, appears)}
-                                    onExit={(node, appears) => exit(node, appears)}
+                                    onEnter={(node, appears) => {
+                                        console.log('onEnter');
+                                        return play(pathname, node, appears)
+                                    }}
+                                    onExit={(node, appears) => {
+                                        console.log('onExit');
+                                        exit(node, appears)
+                                    }}
                                     timeout={{enter: 750, exit: 150}}
                                 >
                                     <Switch location={pathname}>
                                         <Route exact path="/" component={dull}/>
                                         <Route path="/eye" component={full}/>
+                                        <Route path="/house" component={hull}/>
                                     </Switch>
                                 </Transition>
                             </TransitionGroup>
--- src/timelines.js
+++ src/timelines.js
@@ -1,25 +1,27 @@
-import { TimelineMax as Timeline, Power1 } from 'gsap';
+import {TimelineMax as Timeline, Power1} from 'gsap';
 
 const getDefaultTimeline = (node, delay) => {
-    const timeline = new Timeline({ paused: true });
+    const timeline = new Timeline({paused: true});
     const content = node.querySelector('.content');
     const contentInner = node.querySelector('.content--inner');
 
+    console.log('getHomeTimeline for:', content)
+    console.log('getHomeTimeline for:', contentInner)
     timeline
-        .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
-        .from(content, 0.15, { autoAlpha: 0, y: 25, ease: Power1.easeInOut })
-        .from(contentInner, 0.15, { autoAlpha: 0, delay: 0.15, ease: Power1.easeIn });
+        .from(node, 0.3, {display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn})
+        .from(content, 0.15, {autoAlpha: 0, y: 25, ease: Power1.easeInOut})
+        .from(contentInner, 0.15, {autoAlpha: 0, delay: 0.15, ease: Power1.easeIn});
 
     return timeline;
 }
 
 const getHomeTimeline = (node, delay) => {
-    const timeline = new Timeline({ paused: true });
+    const timeline = new Timeline({paused: true});
     const texts = node.querySelectorAll('h1 > div');
-console.log('getHomeTimeline for:', node)
+    console.log('getHomeTimeline for:', texts)
     timeline
-        .from(node, 0, { display: 'none', autoAlpha: 0, delay })
-        .staggerFrom(texts, 0.375, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125);
+        .from(node, 0, {display: 'none', autoAlpha: 0, delay})
+        .staggerFrom(texts, 0.375, {autoAlpha: 0, x: -25, ease: Power1.easeOut}, 0.125);
 
     return timeline;
 };
@@ -29,20 +31,28 @@ export const play = (pathname, node, appears) => {
     let timeline;
     console.log('play-ing:', pathname, node)
 
-    if (pathname.pathname === '/')
+    //if (pathname.pathname === '/')
         timeline = getHomeTimeline(node, delay);
-    else
-        timeline = getDefaultTimeline(node, delay);
+    //else
+    //    timeline = getDefaultTimeline(node, delay);
+
+    console.log('play has a timeline', timeline)
+    //timeline.play();
 
     window
         .loadPromise
-        .then(() => requestAnimationFrame(() => timeline.play()))
+        .then(() => requestAnimationFrame(() => {
+                console.log('loaded promise play:', timeline);
+                return timeline.play();
+            }
+        ))
 }
 
 export const exit = (node) => {
-    const timeline = new Timeline({ paused: true });
+    console.log('exit-ing:', node)
+    const timeline = new Timeline({paused: true});
 
     console.log('exit-ing:', node)
-    timeline.to(node, 0.15, { autoAlpha: 0, ease: Power1.easeOut });
+    timeline.to(node, 0.15, {autoAlpha: 0, ease: Power1.easeOut});
     timeline.play();
 }
